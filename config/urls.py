6e33f6059757b2f7697c7f08a5661865ff066081
abbosbekkukanov <abbosbekkukanov500@gmail.com>
2025-01-13 15:36:42 +0500

    add 13.01.2025 > 15:36

--- config/urls.py
+++ config/urls.py
@@ -10,4 +10,4 @@ urlpatterns = [
     path("main/", include('main.urls'))
 ]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)  
